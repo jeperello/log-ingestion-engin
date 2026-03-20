1ae1e190d78e446f1c9bfcb03bfe62aa65756fee
jeperello <jorgeperello2020@gmail.com>
2026-03-20 19:53:59 -0300

    chore: initial spring boot project structure

---
+++ src/main/java/com/virtualthread/log_ingestion_engine/LogIngestionEngineApplication.java
@@ -0,0 +1,13 @@
+package com.virtualthread.log_ingestion_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogIngestionEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogIngestionEngineApplication.class, args);
+	}
+
+}
